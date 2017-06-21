08903ec756031af4a07eb319adf0575ad29e01ff
jonathanandrewsuk <jonathanandrewsuk@gmail.com>
2017-06-20 20:56:52 -0400

    part way

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import SearchBar from './components/search_bar.js'
+import YTSearch from 'youtube-api-search'
+import VideoList from './components/video_list.js'
+import VideoDetail from './components/video_detail'
+const API_KEY = 'AIzaSyDVJpHONOELQRF4iMHXmqCBqhySorNRoGc'
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+class App extends Component {
+  constructor(props){
+    super(props)
+
+      this.state = { videos: [] }
+
+      YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+        this.setState({ videos })
+      })
+  }
+
+  render(){
+    return (
+      <div>
+        < SearchBar />
+        < VideoDetail video={this.state.videos[0]} />
+        < VideoList videos={this.state.videos} />
+
+      </div>
+    )
+  }
+
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'))
